e458c2da17dd61fba9b0d91b5e2b756bcacf012f
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-02 20:23:14 +0100

    feat(4): strengthen reduced motion policy

--- lib/core/motion/pulse_motion_policy.dart
+++ lib/core/motion/pulse_motion_policy.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class PulseMotionPolicy {
   static bool userReducedMotion = false;
 
+  static bool isReducedMotion(BuildContext context) =>
+      MediaQuery.of(context).disableAnimations || userReducedMotion;
+
   static Duration duration(BuildContext context, Duration normal) =>
-      (MediaQuery.of(context).disableAnimations || userReducedMotion) ? Duration.zero : normal;
+      isReducedMotion(context) ? Duration.zero : normal;
+
+  static Curve curve(BuildContext context, {Curve normal = Curves.easeOutCubic}) =>
+      isReducedMotion(context) ? Curves.linear : normal;
+
+  static Duration microDuration(BuildContext context) =>
+      duration(context, const Duration(milliseconds: 120));
+
+  static Duration transitionDuration(BuildContext context) =>
+      duration(context, const Duration(milliseconds: 260));
 }
