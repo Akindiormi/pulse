0bf672914e5ce8222ceabc8aaf61171042d01f4d
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-02 18:15:40 +0100

    feat: formalize motion and analytics event names

--- lib/core/motion/pulse_events.dart
+++ lib/core/motion/pulse_events.dart
@@ -1,7 +1,42 @@
-sealed class PulseEvent { const PulseEvent(); }
-class ActivityCompletedEvent extends PulseEvent { const ActivityCompletedEvent({required this.xpAwarded, required this.newXP, required this.previousStreak, required this.newStreak, required this.newAchievements, required this.leveledUp}); final int xpAwarded, newXP, previousStreak, newStreak; final List<String> newAchievements; final bool leveledUp; }
-class StreakIncreasedEvent extends PulseEvent { const StreakIncreasedEvent({required this.previous, required this.current}); final int previous, current; }
-class AchievementUnlockedEvent extends PulseEvent { const AchievementUnlockedEvent(this.achievementId); final String achievementId; }
-class LevelUpEvent extends PulseEvent { const LevelUpEvent({required this.previousLevel, required this.newLevel}); final int previousLevel, newLevel; }
-class DailyChallengeRefreshedEvent extends PulseEvent { const DailyChallengeRefreshedEvent(this.challengeId); final String challengeId; }
-class ShareCardGeneratedEvent extends PulseEvent { const ShareCardGeneratedEvent(this.data); final Object data; }
+sealed class PulseEvent {
+  const PulseEvent();
+  String get analyticsName;
+}
+
+class ActivityCompletedEvent extends PulseEvent {
+  const ActivityCompletedEvent({required this.xpAwarded, required this.newXP, required this.previousStreak, required this.newStreak, required this.newAchievements, required this.leveledUp});
+  final int xpAwarded, newXP, previousStreak, newStreak;
+  final List<String> newAchievements;
+  final bool leveledUp;
+  @override String get analyticsName => 'challenge_completed';
+}
+
+class StreakIncreasedEvent extends PulseEvent {
+  const StreakIncreasedEvent({required this.previous, required this.current});
+  final int previous, current;
+  @override String get analyticsName => 'streak_increased';
+}
+
+class AchievementUnlockedEvent extends PulseEvent {
+  const AchievementUnlockedEvent(this.achievementId);
+  final String achievementId;
+  @override String get analyticsName => 'achievement_unlocked';
+}
+
+class LevelUpEvent extends PulseEvent {
+  const LevelUpEvent({required this.previousLevel, required this.newLevel});
+  final int previousLevel, newLevel;
+  @override String get analyticsName => 'level_up';
+}
+
+class DailyChallengeRefreshedEvent extends PulseEvent {
+  const DailyChallengeRefreshedEvent(this.challengeId);
+  final String challengeId;
+  @override String get analyticsName => 'challenge_viewed';
+}
+
+class ShareCardGeneratedEvent extends PulseEvent {
+  const ShareCardGeneratedEvent(this.data);
+  final Object data;
+  @override String get analyticsName => 'share_card_created';
+}
