a28e9d47b62ed14cf400e73d2ab89a2ccbcf587d
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-02 18:14:56 +0100

    feat: initialize Firebase at application startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'core/firebase/firebase_bootstrap.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await const DefaultFirebaseBootstrap().initialize();
   runApp(const ProviderScope(child: PulseApp()));
 }
