8c49772420ac51c018b047953a5c0ef44d61fee5
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-03 00:50:27 +0100

    fix: remove unused test import

--- test/core/ui_foundation_test.dart
+++ test/core/ui_foundation_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:pulse/core/design/pulse_tokens.dart';
 import 'package:pulse/core/theme/app_theme.dart';
