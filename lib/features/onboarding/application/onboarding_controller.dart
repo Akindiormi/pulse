7a4d1e9108558229a7ad19c9bd1739a6c2499530
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-02 20:05:38 +0100

    phase 3g: add persisted onboarding state

---
+++ lib/features/onboarding/application/onboarding_controller.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../core/telemetry/analytics_service.dart';
+import '../../../core/di/providers.dart';
+
+enum OnboardingMotionState { entering, active, exiting, skipped, completed }
+
+class OnboardingController extends AsyncNotifier<bool> {
+  static const _key = 'pulse.onboarding_completed';
+
+  @override
+  Future<bool> build() async => (await SharedPreferencesAsync().getBool(_key)) ?? false;
+
+  Future<void> complete() async {
+    await SharedPreferencesAsync().setBool(_key, true);
+    state = const AsyncData(true);
+    await ref.read(analyticsServiceProvider).logOnboardingCompleted();
+  }
+
+  Future<void> skip() async {
+    await SharedPreferencesAsync().setBool(_key, true);
+    state = const AsyncData(true);
+    await ref.read(analyticsServiceProvider).logOnboardingSkipped();
+  }
+}
+
+final onboardingControllerProvider = AsyncNotifierProvider<OnboardingController, bool>(OnboardingController.new);
