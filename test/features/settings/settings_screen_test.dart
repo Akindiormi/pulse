095a4ce9cca32e3babd5d7970ce14fae6c2fc413
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-09 09:18:09 +0100

    test: make settings account action interaction reliable

--- test/features/settings/settings_screen_test.dart
+++ test/features/settings/settings_screen_test.dart
@@ -49,8 +49,10 @@ void main() {
     final controller = FakeSettingsController(initial);
     await tester.pumpWidget(_app(controller));
     await tester.pump();
-    await tester.scrollUntilVisible(find.text('sign out'), 300);
-    await tester.tap(find.text('sign out'));
+    final signOut = find.text('sign out');
+    await tester.ensureVisible(signOut);
+    await tester.pump();
+    await tester.tap(signOut);
     await tester.pumpAndSettle();
     expect(find.text('sign out?'), findsOneWidget);
     expect(controller.signOutCalls, 0);
