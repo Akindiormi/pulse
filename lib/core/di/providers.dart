48cbd157fb75c5c239d4fba5d2ca2870d6886c14
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-09 08:50:20 +0100

    phase 2: switch dependency injection to Supabase auth/database/backend

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,16 +1,14 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../auth/auth_service.dart';
 import '../backend/trusted_account_backend.dart';
 import '../backend/trusted_challenge_backend.dart';
-import '../database/firestore_repositories.dart';
 import '../database/repositories.dart';
+import '../database/supabase_repositories.dart';
 import '../motion/pulse_event_dispatcher.dart';
 import '../notifications/firebase_notification_service.dart';
 import '../notifications/notification_service.dart';
@@ -21,25 +19,23 @@ import '../telemetry/firebase_crash_reporter.dart';
 import '../../features/achievements/application/achievement_service.dart';
 import '../../features/challenges/application/challenge_service.dart';
 import '../../features/challenges/application/complete_challenge.dart';
-import '../../services/firebase_auth_service.dart';
+import '../../services/supabase_auth_service.dart';
 
-final firestoreProvider = Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
-final functionsProvider = Provider<FirebaseFunctions>((ref) => FirebaseFunctions.instanceFor(region: 'us-central1'));
-final authProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+final supabaseProvider = Provider<SupabaseClient>((ref) => Supabase.instance.client);
 final analyticsProvider = Provider<FirebaseAnalytics>((ref) => FirebaseAnalytics.instance);
 final messagingProvider = Provider<FirebaseMessaging>((ref) => FirebaseMessaging.instance);
 final crashlyticsProvider = Provider<FirebaseCrashlytics>((ref) => FirebaseCrashlytics.instance);
 
-final trustedCallableClientProvider = Provider<TrustedCallableClient>((ref) => FirebaseTrustedCallableClient(ref.watch(functionsProvider)));
-final trustedAccountBackendProvider = Provider<TrustedAccountBackend>((ref) => FirebaseCallableAccountBackend(ref.watch(trustedCallableClientProvider)));
-final authServiceProvider = Provider<AuthService>((ref) => FirebaseAuthService(ref.watch(authProvider), ref.watch(trustedAccountBackendProvider)));
+final trustedCallableClientProvider = Provider<TrustedCallableClient>((ref) => SupabaseTrustedCallableClient(ref.watch(supabaseProvider)));
+final trustedAccountBackendProvider = Provider<TrustedAccountBackend>((ref) => SupabaseCallableAccountBackend(ref.watch(trustedCallableClientProvider)));
+final authServiceProvider = Provider<AuthService>((ref) => SupabaseAuthService(ref.watch(supabaseProvider), ref.watch(trustedAccountBackendProvider)));
 final authStateProvider = StreamProvider<AuthState>((ref) => ref.watch(authServiceProvider).authStateChanges);
-final userRepositoryProvider = Provider<UserRepository>((ref) => FirestoreUserRepository(ref.watch(firestoreProvider)));
-final challengeRepositoryProvider = Provider<ChallengeRepository>((ref) => FirestoreChallengeRepository(ref.watch(firestoreProvider)));
-final activityRepositoryProvider = Provider<ActivityRepository>((ref) => FirestoreActivityRepository(ref.watch(firestoreProvider)));
-final achievementRepositoryProvider = Provider<AchievementRepository>((ref) => FirestoreAchievementRepository(ref.watch(firestoreProvider)));
+final userRepositoryProvider = Provider<UserRepository>((ref) => SupabaseUserRepository(ref.watch(supabaseProvider)));
+final challengeRepositoryProvider = Provider<ChallengeRepository>((ref) => SupabaseChallengeRepository(ref.watch(supabaseProvider)));
+final activityRepositoryProvider = Provider<ActivityRepository>((ref) => SupabaseActivityRepository(ref.watch(supabaseProvider)));
+final achievementRepositoryProvider = Provider<AchievementRepository>((ref) => SupabaseAchievementRepository(ref.watch(supabaseProvider)));
 
-final trustedChallengeBackendProvider = Provider<TrustedChallengeBackend>((ref) => FirebaseCallableChallengeBackend(ref.watch(trustedCallableClientProvider), ref.watch(authServiceProvider)));
+final trustedChallengeBackendProvider = Provider<TrustedChallengeBackend>((ref) => SupabaseTrustedChallengeBackend(ref.watch(trustedCallableClientProvider), ref.watch(authServiceProvider)));
 final achievementServiceProvider = Provider<AchievementService>((ref) => const AchievementService());
 final challengeServiceProvider = Provider<ChallengeService>((ref) => ChallengeService(repository: ref.watch(challengeRepositoryProvider), backend: ref.watch(trustedChallengeBackendProvider)));
 final completeChallengeProvider = Provider<CompleteChallenge>((ref) => CompleteChallenge(backend: ref.watch(trustedChallengeBackendProvider)));
