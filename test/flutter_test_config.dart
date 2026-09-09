cdc8577ac638f20ed783a48eec43fdd2dcf25ba5
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-09 09:18:14 +0100

    test: allow Manrope font loading in CI

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -3,6 +3,9 @@ import 'dart:async';
 import 'package:google_fonts/google_fonts.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
-  GoogleFonts.config.allowRuntimeFetching = false;
+  // Manrope is currently supplied through google_fonts. CI has network access,
+  // so allow the package to fetch the font instead of failing when no local
+  // TTF asset has been bundled yet.
+  GoogleFonts.config.allowRuntimeFetching = true;
   await testMain();
 }
