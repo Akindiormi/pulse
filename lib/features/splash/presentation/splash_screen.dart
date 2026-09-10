3fd84f5891d9162e6568950f44b856ba674119fb
Akindiormi <141813019+Akindiormi@users.noreply.github.com>
2026-09-10 16:38:52 +0100

    fix(startup): repair splash widget syntax

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import '../../../core/design/pulse_tokens.dart';
 import '../../../core/errors/app_error.dart';
 import '../../../core/theme/app_colors.dart';
+import '../../../core/design/pulse_tokens.dart';
 import '../../../core/motion/pulse_motion_policy.dart';
 import '../application/splash_controller.dart';
 
